c4c865f0ea6526dd6eb81cd910ea7d698cde9a29
khoa300504 <khoa300504@gmail.com>
2024-04-20 09:35:32 +0700

    Environment Variable Config

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,28 @@
-//MongoDb
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment'
+
+let instagramDatabaseInstance = null
+
+const client = new MongoClient(env.MONGODB_URI,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    }
+  }
+)
+
+export const CONNECT_DB = async () => {
+  await client.connect()
+  instagramDatabaseInstance = client.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!instagramDatabaseInstance) throw new Error('Connect to Database first!')
+  return instagramDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await instagramDatabaseInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,25 +1,48 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from './config/mongodb'
+import existHook from 'async-exit-hook'
+import { env } from './config/environment'
 
-const app = express()
+const START_SEVER = () => {
+  const hostname = 'localhost'
+  const port = 8017
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    console.log(`3.Backend server is running at http://${ hostname }:${ port }/`)
+  })
 
-app.listen(port, hostname, () => {
-  console.log(`I am running at ${ hostname }:${ port }/`)
-})
+  existHook(() => {
+    console.log('4.Disconnecting from MongoDb Clound Atlas...')
+    CLOSE_DB()
+    console.log('5.Disconnected from MongoDb Clound Atlas')
+  })
+}
+
+(async () => {
+  try {
+    console.log('1.Connecting to MongoDB Clound Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDb Atlas!')
+
+    START_SEVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// console.log('1.Connecting to MongoDB Clound Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDb Atlas!'))
+//   .then(() => START_SEVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
